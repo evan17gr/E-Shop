200ad6277b51abf794d79361f9e2fca93de8d727
Evan Kalvis <evankalvis@hotmail.com>
2020-11-06 01:20:17 +0000

    Added redux to the project

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from "react-redux";
+import store from "./store";
 import App from './App';
 import "./index.css";
 import "./bootstrap.min.css";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-   <App />,
+   <Provider store={store}>
+      <App />
+   </Provider>,
   document.getElementById('root')
 );
 
--- frontend/src/pages/ProductScreen.js
+++ frontend/src/pages/ProductScreen.js
@@ -1,27 +1,34 @@
 import React,{useState,useEffect} from "react";
 import {Link} from "react-router-dom";
-import {Row,Col,Image,Card,Button,ListGroup} from "react-bootstrap";
+import {useDispatch,useSelector} from "react-redux";
+import {Row,Col,Image,Card,Button,ListGroup, Form} from "react-bootstrap";
 import Rating from "../components/Rating";
-import axios from "axios";
+import {listProductDetails} from "../actions/productsActions";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
-const ProductScreen = ({match}) => {
-    const [product,setProduct] = useState({});
+const ProductScreen = ({match,history}) => {
+    const [qty,setQty] = useState(0);
+
+    const addToCart = () => {
+        history.push(`/cart/${match.params.id}?qty=${qty}`)
+    }
+
+    const dispatch = useDispatch();
+
+    const productDetails = useSelector(state => state.productDetails);
+    const {loading,error,product} = productDetails;
 
     useEffect(() => {
-        const fetchProduct = async () => {
-            const {data} = await axios.get(`/api/products/${match.params.id}`);
-
-            setProduct(data);
-        }
-
-        fetchProduct();
-    },[])
+        dispatch(listProductDetails(match.params.id))
+    },[dispatch, match])
 
     return <>
         <Link className="btn btn-light my-3" to="/">
             Go Back
         </Link>
-        <Row>
+        {loading ? <Loader/> :error ? <Message variant="danger">{error}</Message> 
+        : (<Row>
             <Col md={6}>
                 <Image src={product.image} alt={product.name} fluid/>
             </Col>
@@ -64,15 +71,35 @@ const ProductScreen = ({match}) => {
                                 </Col>
                             </Row>
                         </ListGroup.Item>
+                        
+                        {product.countInStock > 0 && (
+                            <ListGroup.Item>
+                                <Row>
+                                   <Col>Qty</Col>
+                                   <Col>
+                                        <Form.Control as="select" value={qty} onChange={(e) => setQty(e.target.value)}>
+                                            {[...Array(product.countInStock).keys()].map(x => (
+                                                <option key={x+1} value={x+1}>
+                                                    {x+1}
+                                                </option>
+                                            ))}
+                                        </Form.Control>
+                                   </Col> 
+                                </Row>
+                            </ListGroup.Item>
+
+                        )}
+
                         <ListGroup.Item>
-                            <Button className="btn-block" type="button" disabled={product.countInStock === 0}>
+                            <Button className="btn-block" type="button" disabled={product.countInStock === 0} onClick={addToCart}>
                                 Add To Cart
                             </Button>            
                         </ListGroup.Item>
                     </ListGroup>
                 </Card>
             </Col>
-        </Row>
+        </Row>)}
+        
     </>
 }
 
